7ea691a923a96d743a25917c77f210e91d05cacc
DavidRod0 <davidsgar08@gmail.com>
2026-08-05 15:24:58 -0500

    create Square class

--- fib.java
+++ fib.java
@@ -3,3 +3,13 @@ public class Square{
     private int xPosition;
     private int yPosition;
 }
+public class Square(){
+    private int base;
+    private String color;
+    private int yPos;
+    private int xPos;
+    public Square( int base, String color ){
+        this.base = base;
+        this.color = color;
+    }
+}
